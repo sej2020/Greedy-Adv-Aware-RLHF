a933f92ebcf228594b729862e4f0a57249505303
Sam Johnson <sejohnson2020@outlook.com>
2024-09-07 17:11:24 -0400

    param update plots

--- plotting/theoretical_grad_updates_plot.py
+++ plotting/theoretical_grad_updates_plot.py
@@ -18,27 +18,27 @@ Z = np.linspace(0, 1, 101)
 X, Z = np.meshgrid(X, Z)
 
 # only delta x*
-# Y = (Z)*(-X/2) + (1-Z)*((1-X/2)**10-1)
-# ax3d.set_ylabel(r'Coefficient for $\frac{\delta J_{x\star}}{\delta \theta}$ update')
+Y = (1-Z)*(-X/2) + (Z)*((1-X/2)**10-1)
+ax3d.set_ylabel("b")
 
 # only delta x
-Y = (Z)*(X/2 + 1) + (1-Z)*((1-X/2)**10)
-ax3d.set_ylabel(r'Coefficient for $\frac{\delta J_{x}}{\delta \theta}$ update')
+# Y = (1-Z)*(X/2 + 1) + (Z)*((1-X/2)**10)
+# ax3d.set_ylabel("a")
 
-ax3d.yaxis.label.set_rotation(90)
+ax3d.yaxis.label.set_rotation(0)
 ax3d.yaxis.label.set_fontsize(15)
 ax3d.yaxis.labelpad = 20
 
 #setting axis titles
-ax3d.set_xlabel('Adv(x*) - Adv(x) in ' + r'$\sigma$')
+ax3d.set_xlabel('A(x*) - A(x) in ' + r'$\sigma$')
 ax3d.xaxis.label.set_fontsize(15)
 ax3d.xaxis.labelpad = 20
-ax3d.set_zlabel('Entropy')
+ax3d.set_zlabel(r'$\pi_{\theta}$' + "(x*)") # prob of greedy selection which has been min max scaled
 ax3d.zaxis.label.set_fontsize(15)
+ax3d.zaxis.label.set_rotation(180)
 ax3d.zaxis.labelpad = 20
 
 #flip z axis
-ax3d.invert_zaxis()
 # setting color as a function of z
 ax3d.plot_surface(X, Y, Z, rstride=5, cstride=5, cmap='viridis')
 
